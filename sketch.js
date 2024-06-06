706d4ebc53da43f78ebf557029d48584297e5025
alissonbig <00001250102601sp@al.educacao.sp.gov.br>
2024-06-06 20:37:20 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
-html, body {
-  margin: 0;
-  padding: 0;
+    fill(color(255,140, 0));
+    rect(450, 10, 40, 20);
+    fill(255);
+    text(pontosDoOponente, 470, 26);
+
+
+
 }
-canvas {
-  display: block;
+
+
+function marcaPonto() {
+  if (xBolinha > 580) {
+    meusPontos += 1;
+  }
+  if (xBolinha < 10) {
+    pontosDoOponente += 1;
+  }
 }
